0bb7d452d662765bb3450f0ebe6ee20a2e18060b
yogesh <yogeshfunde@gmail.com>
2022-08-11 20:16:38 +0530

    first commit

--- project.js
+++ project.js
@@ -1,7 +1,21 @@
 const http = require("http");
-const User = require('./user');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'content-type':'application\json'});
+const User = require("./user");
+const fs = require("fs");
+http.createServer((req, resp) => {
+    resp.writeHead(200, { "content-type": "applicationjson" });
     resp.write(JSON.stringify(User));
+    
+
+    if ((req.url = "/userapi")) {
+      fs.readFile(`${__dirname}/userapi.json`, "utf-8", (err, data) => {
+        console.log(data);
+      });
+      resp.end("Hello from the user api sides");
+    } else {
+      resp.writeHead(200, { "content-type": "applicationjson" });
+      resp.end("<h2> 200 error page. page doesn't exist </h1>");
+    }
     resp.end();
-}).listen(5000);
+  })
+  .listen(5000);
+console.log("server started at port 5000");
